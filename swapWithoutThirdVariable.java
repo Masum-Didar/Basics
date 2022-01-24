c73a37a9b3bbc0f5f44671bd1dc0ce81744eae38
Masum-Didar <96762879+Masum-Didar@users.noreply.github.com>
2022-01-24 18:19:16 +0600

    Create swapWithoutThirdVariable.java

---
+++ swapWithoutThirdVariable.java
@@ -0,0 +1,10 @@
+public calss swapWithoutThirdVariable(){
+  int x, y;
+  x=10;
+  y=20;
+  System.out.println("Before Swaping X ="+x+" y="+y);
+  x=x+y;
+  y=x-y;
+  x=x-y;
+  System.out.println("Before Swaping X ="+x+" y="+y);
+}
