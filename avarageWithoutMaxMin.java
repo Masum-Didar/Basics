55b1370bccd42e96e92e250acf20399af0387d53
Abu Masum Didar <96762879+Masum-Didar@users.noreply.github.com>
2022-10-30 15:56:43 +0600

    Update avarageWithoutMaxMin.java

--- avarageWithoutMaxMin.java
+++ avarageWithoutMaxMin.java
@@ -1,43 +1,29 @@
-
 package interview;
-
-
 public class avarageWithoutMaxMin {
     
     public static void main(String[] args) {
-    
-    int[] arr={3, 5, 6, 8, 1, 7, 9};
-    int max = arr[0];
-    int min = arr[arr.length-1];
-    int sum = 0;
-     
-    
-        for(int i = 1; i<arr.length; i++){
-
-                if(max<arr[i]){
-                    max=arr[i];
-                }
-                if(min>arr[i]){
-                    min=arr[i];
-                }
-
-            }
-            for(int j=0; j<arr.length; j++){
-                if(max != arr[j] && min !=arr[j]){
-                    sum = sum + arr[j];
-
-                System.out.print(arr[j]+" ");
-                }
-            }
-            System.out.println();
-            System.out.println("The max number is "+max);
-            System.out.println("The min number is "+min);
-            System.out.println("The sum number is "+sum);
-             float avg = sum % (arr.length-2);
-             System.out.println("The avarage is "+avg);
+		int arr[] = {1, 25, 3,58,35,46,99};
+		int max = arr[0], min=arr[arr.length-1];
+		float sum=0, Average;
         
-    }
-    
-   
-    
+		for(int i=0; i<arr.length; i++) {
+			if(max<arr[i]) { 
+				max = arr[i];
+			}
+			if(min>arr[i]) {
+				min = arr[i];
+			}
+		}
+		for(int j=0; j<arr.length; j++) {
+			if(max !=arr[j] && min != arr[j]) {
+				sum = sum + arr[j];
+				System.out.print(arr[j]+" ");
+			}
+		}
+		Average = sum/(arr.length-2);
+		System.out.println();
+		System.out.println("Sum = "+sum);
+		System.out.println("Average = "+Average);
+		System.out.println(min + " "+max);	
+	}
 }
