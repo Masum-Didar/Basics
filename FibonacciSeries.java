afd347442ac816d50c9f2c8b2750930f2a980b4b
Abu Masum Didar <96762879+Masum-Didar@users.noreply.github.com>
2022-10-30 16:30:02 +0600

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+package Class_9;
+
+public class FibonacciSeries {
+	public static int fibonacciRecursion(int n){
+		if(n == 0){
+			return 0;
+		}
+		if(n == 1 || n == 2){
+				return 1;
+			}
+		return fibonacciRecursion(n-2) + fibonacciRecursion(n-1); // logic link https://www.guru99.com/fibonacci-series-java.html
+		}
+
+	public static void main(String[] args) {
+		
+		int maxNumber = 10;
+		System.out.print("Fibonacci Series of "+maxNumber+" numbers: ");
+		
+		for(int i = 0; i < maxNumber; i++){
+				System.out.print(fibonacciRecursion(i) +" ");
+			}
+		}
+}
