d69d58b223d6d537a58a26df9c4d93172affb5e4
Abu Masum Didar <96762879+Masum-Didar@users.noreply.github.com>
2022-10-30 15:33:04 +0600

    Update primeNumber.java

--- primeNumber.java
+++ primeNumber.java
@@ -12,18 +12,23 @@ public class primeNumber {
         
         System.out.print("Enter Your Integer Number: ");
         int n = sc.nextInt();
+        int count = 0;
         if(n==1){
             System.out.println("Enter a number Greater than 1");
         }
         
          for(int i=2; i<n; i++){
                 if(n%i==0){
-                          System.out.println("The number is not prime");
+                	   count++;   
                        break;
-                }else
-                  System.out.println("The number is prime");
-                break;
-      } 
+                }
+       }
+         if(count==0) {
+        	 System.out.println("The number is prime");
+         }else
+        	 System.out.println("The number is not prime");
+          
+        
     }
     
 }
